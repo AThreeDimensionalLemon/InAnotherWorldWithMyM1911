ce791ef0f169485d821cade769bc3b97305a6b82
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-06-02 12:27:38 -0700

    Added shooting bullets

--- src/Groups/BulletGroup.js
+++ src/Groups/BulletGroup.js
@@ -2,7 +2,11 @@ class BulletGroup extends Phaser.Physics.Arcade.Group {
     constructor(scene) {
 
         //setup parent class
-        super(scene.physics.world, scene);
+        super(scene.physics.world, scene, {
+            key: "sprite_bullet",
+            classType: Bullet,
+            max: 20
+        });
         scene.add.existing(this);
         scene.physics.add.existing(this);
     }
